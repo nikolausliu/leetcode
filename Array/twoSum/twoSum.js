81f78cd355d854e06744be652c598d601e1c3e6d
nikolausliu <18010855044@163.com>
2018-07-11 15:18:43 +0800

    findMedianSortedArrays

--- Array/twoSum/twoSum.js
+++ Array/twoSum/twoSum.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = (nums, target) => {
   for (var i = 0; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; j++) {
